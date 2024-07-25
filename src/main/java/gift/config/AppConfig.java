f50fce61aabcadd8e42ed3b732ed5ae74086a5c1
mogld <143364802+mogld@users.noreply.github.com>
2024-07-25 23:13:33 +0900

    피드백 적용

--- src/main/java/gift/config/AppConfig.java
+++ src/main/java/gift/config/AppConfig.java
@@ -1,14 +1,35 @@
 package gift.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(ObjectMapper objectMapper) {
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter(objectMapper));
+        return restTemplate;
+    }
+
+    @Bean
+    public SimpleClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        return factory;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule()); // JavaTimeModule 등록
+        return mapper;
     }
 }
